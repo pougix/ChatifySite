4f1c4c5782a82510ae8cda4564d3f244848db831
Jean-Bapthiste Pouget <j.bapthiste@letsdoyourplan.com>
2023-06-05 11:12:56 +0700

    wip

--- database/migrations/2023_05_20_074521_create_embed_collections_table.php
+++ database/migrations/2023_05_20_074521_create_embed_collections_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('embed_collections', function (Blueprint $table) {
             $table->uuid("id")->primary();
-            $table->string("name");
+            $table->text("name");
             $table->text("meta_data");
             $table->timestamps();
         });
